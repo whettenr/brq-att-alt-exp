6c9415880230b30c3ddcb02e1873e1065babbafa
whettenr <whettenrmw@gmail.com>
2024-02-09 15:24:18 +0100

    add num_encoder layer parameter

--- run/finetune/run_hc_lg.sh
+++ run/finetune/run_hc_lg.sh
@@ -19,18 +19,22 @@ encoder_module='conformer'
 output_folder='results/ft/hc_lg'
 hub=/gpfswork/rech/nkp/uaj64gk/attention_alt/brq-att-alt-exp/results/hc_lg/1000/save/CKPT+2024-02-07+02-50-50+00
 data_folder=/gpfsdswork/dataset/LibriSpeechAsrCorpus
+num_encoder_layers=24
 
 python finetune/ft_brq.py finetune/ft_brq.yaml \
     --data_folder $data_folder \
     --pt_model_hub $hub \
     --pt_model_output_dim $encoder_dim \
     --attention_type $attention_type --encoder_module $encoder_module \
+    --num_encoder_layers $num_encoder_layers \
     --output_folder $output_folder
 
+
 python finetune/ft_brq.py finetune/ft_brq.yaml \
     --data_folder $data_folder \
     --pt_model_hub $hub \
     --pt_model_output_dim $encoder_dim \
     --attention_type $attention_type --encoder_module $encoder_module \
+    --num_encoder_layers $num_encoder_layers \
     --output_folder $output_folder \
     --test_only --kenlm_model_path /gpfswork/rech/nkp/uaj64gk/bestrqexp/4-gram.arpa.gz
