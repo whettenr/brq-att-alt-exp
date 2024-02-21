23332591ef57267e6e03816351f4d138c16e9973
whettenr <whettenrmw@gmail.com>
2024-02-21 18:06:25 +0100

    update run

--- run_can/ff_lg_test/run_ff_lg.sh
+++ run_can/ff_lg_test/run_ff_lg.sh
@@ -2,11 +2,11 @@
 
 #SBATCH --job-name=ff_lg   # nom du job
 #SBATCH --account=rrg-ravanelm
-#SBATCH --gres=gpu:2
+#SBATCH --gres=gpu:1
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=16
 #SBATCH --exclusive
-#SBATCH --time=20:00:00          # temps d'exécution maximum demande (HH:MM:SS) 
+#SBATCH --time=10:00:00          # temps d'exécution maximum demande (HH:MM:SS) 
 #SBATCH --output=log/ff_lg_%j.log  # log file
 
 # load env
@@ -28,6 +28,6 @@ tar -zxf train-other-500.tar.gz
 cd  $HOME/alt_att/brq-att-alt-exp
 
 python -m torch.distributed.run --nproc_per_node=1 --rdzv_backend c10d --rdzv-endpoint=localhost:0 train.py hparams/fastformer_lg.yaml --find_unused_parameters \
-    --grad_accumulation_factor 8 --output_folder results/ff_lg_test/ff_lg_branch \
+    --grad_accumulation_factor 16 --output_folder results/ff_lg_test/ff_lg_branch \
     --d_model 1184 --encoder_module branchformer --transformer_dropout 0.2 \
     --data_folder $SLURM_TMPDIR/librispeech/LibriSpeech 
