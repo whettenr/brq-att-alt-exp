deb5d336e49c865646cc1b869797cfbf2fa115b2
whettenr <whettenrmw@gmail.com>
2024-02-22 14:32:17 +0100

    remove debug and update ckpt

--- run/ds/slurp/run_bench_brq.sh
+++ run/ds/slurp/run_bench_brq.sh
@@ -12,9 +12,9 @@ module load pytorch-gpu/py3/2.1.1
 conda activate aa
 cd /gpfswork/rech/nkp/uaj64gk/attention_alt/brq-att-alt-exp
 
-hub=/gpfswork/rech/nkp/uaj64gk/attention_alt/brq-att-alt-exp/results/old/brq/1000/save/CKPT+2024-02-01+05-33-06+00
+hub=/gpfswork/rech/nkp/uaj64gk/attention_alt/brq-att-alt-exp/results/brq/1000/save/CKPT+2024-02-18+02-51-05+00
 num_layers='13'
-encoder_dim='512' # change this to 576
+encoder_dim='576'
 output_folder='results/MP3/brq'
 csv_location=/gpfswork/rech/nkp/uaj64gk/attention_alt/brq-att-alt-exp/results/MP3S/csv/SLURP
 benchmark_location=/gpfswork/rech/nkp/uaj64gk/attention_alt/benchmarks
@@ -28,19 +28,7 @@ for i in "${!ConsideredTasks[@]}"; do
 	downstream=${DownStreams[i]}
 	dataset_folder=${DatasetsFolders[i]}
 	python $benchmark_location/benchmarks/MP3S/$task/$downstream/train.py $benchmark_location/benchmarks/MP3S/$task/$downstream/hparams/ssl_brq.yaml \
-		--num_layers_ssl $num_layers --ssl_hub $hub --encoder_dim $encoder_dim --output_folder $output_folder/$task/$downstream --data_folder $dataset_folder --debug \
+		--num_layers_ssl $num_layers --ssl_hub $hub --encoder_dim $encoder_dim --output_folder $output_folder/$task/$downstream --data_folder $dataset_folder \
 		--csv_location $csv_location
 done
-
-
-# DatasetsFolders=( '/corpus/LibriSpeech/' '/corpus/LibriSpeech/' '/local_disk/arges/jduret/corpus/voxceleb2' '/users/rwhetten/benchmarks/benchmarks/MP3S/SLURP')
-# ConsideredTasks=('LibriSpeech' 'LibriSpeech' 'VoxCeleb1' 'SLURP')
-# DownStreams=('contextnet' 'LSTM' 'ecapa_tdnn' 'LSTM_linear')
-
-# for i in "${!ConsideredTasks[@]}"; do
-# 	task=${ConsideredTasks[i]}
-# 	downstream=${DownStreams[i]}
-# 	dataset_folder=${DatasetsFolders[i]}
-# 	python /users/rwhetten/benchmarks/benchmarks/MP3S/$task/$downstream/train.py /users/rwhetten/benchmarks/benchmarks/MP3S/$task/$downstream/hparams/ssl_brq.yaml --num_layers_ssl $num_layers --ssl_hub $hub --encoder_dim $encoder_dim --output_folder $output_folder/$task/$downstream --data_folder $dataset_folder --debug
-# done
 
