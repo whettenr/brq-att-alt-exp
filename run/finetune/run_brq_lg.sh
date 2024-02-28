73bfe61781ebc9cab360e610044ef6756b89ae6e
whettenr <whettenrmw@gmail.com>
2024-02-28 15:38:08 +0100

    update finetune run script

--- run/finetune/run_brq_lg.sh
+++ run/finetune/run_brq_lg.sh
@@ -14,19 +14,22 @@ conda activate aa
 
 cd /gpfswork/rech/nkp/uaj64gk/attention_alt/brq-att-alt-exp
 
+hub=/gpfswork/rech/nkp/uaj64gk/attention_alt/brq-att-alt-exp/results/brq_lg/1000/save/CKPT+2024-02-28+09-05-17+00
+encoder_dim='848'
+
 
 python finetune/ft_brq.py finetune/ft_brq.yaml \
     --data_folder /gpfsdswork/dataset/LibriSpeechAsrCorpus \
-    --pt_model_hub /gpfswork/rech/nkp/uaj64gk/attention_alt/brq-att-alt-exp/results/brq_lg/1000/save/CKPT+2024-02-02+05-07-26+00 \
-    --pt_model_output_dim 768 \
+    --pt_model_hub $hub \
+    --pt_model_output_dim $encoder_dim \
     --num_encoder_layers 24 \
     --output_folder results/ft/brq_lg
 
 
 python finetune/ft_brq.py finetune/ft_brq.yaml \
     --data_folder /gpfsdswork/dataset/LibriSpeechAsrCorpus \
-    --pt_model_hub /gpfswork/rech/nkp/uaj64gk/attention_alt/brq-att-alt-exp/results/brq_lg/1000/save/CKPT+2024-02-02+05-07-26+00 \
-    --pt_model_output_dim 768 \
+    --pt_model_hub $hub \
+    --pt_model_output_dim $encoder_dim \
     --num_encoder_layers 24 \
     --output_folder results/ft/brq_lg \
     --test_only --kenlm_model_path /gpfswork/rech/nkp/uaj64gk/bestrqexp/4-gram.arpa.gz
