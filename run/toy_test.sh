2ffd85832f5fd37cc78c70eb3aad6cfb6d921bbf
whettenr <whettenrmw@gmail.com>
2024-03-01 14:38:18 +0100

    shrink down

--- run/toy_test.sh
+++ run/toy_test.sh
@@ -17,10 +17,37 @@ cd /gpfswork/rech/nkp/uaj64gk/attention_alt/brq-att-alt-exp
 for sim_test_time in 10 20 30 40 50 60
 do
     torchrun --nproc_per_node=4 --rdzv_backend c10d --rdzv-endpoint=localhost:0 \
+        memory_test.py hparams/fastformer.yaml --find_unused_parameters \
+        --seconds_per_batch 100 --train_num_buckets 50 \
+        --grad_accumulation_factor 1 --precision fp16 --optimizer_step_limit 1000 \
+        --sim_test_time $sim_test_time --sim_batch_size 2 \
+        --log_interval 100 \
+        --output_folder results/toy/ff/ \
+        --d_model 624 --encoder_module conformer --nhead 16 --transformer_dropout 0.2 --num_encoder_layers 12
+    rm -r results/toy/ff/save
+done
+
+
+for sim_test_time in 10 20 30 40 50 60
+do
+    torchrun --nproc_per_node=4 --rdzv_backend c10d --rdzv-endpoint=localhost:0 \
+        memory_test.py hparams/fastformer_lg.yaml --find_unused_parameters \
+        --seconds_per_batch 100 --train_num_buckets 50 \
+        --grad_accumulation_factor 1 --precision fp16 --optimizer_step_limit 1000 \
+        --sim_test_time $sim_test_time --sim_batch_size 2 \
+        --log_interval 100 \
+        --output_folder results/toy/ff_lg/ \
+        --d_model 1472 --encoder_module conformer --nhead 32 --transformer_dropout 0.2 --num_encoder_layers 12 
+    rm -r results/toy/ff_lg/save
+done
+
+for sim_test_time in 10 20 30 40 50 60
+do
+    torchrun --nproc_per_node=4 --rdzv_backend c10d --rdzv-endpoint=localhost:0 \
         memory_test.py hparams/brq.yaml --find_unused_parameters \
         --seconds_per_batch 100 --train_num_buckets 50 \
         --grad_accumulation_factor 1 --precision fp16 --optimizer_step_limit 1000 \
-        --sim_test_time $sim_test_time --sim_batch_size 10 \
+        --sim_test_time $sim_test_time --sim_batch_size 2 \
         --log_interval 100 \
         --output_folder results/toy/brq/
     rm -r results/toy/brq/save
@@ -32,7 +59,7 @@ do
         memory_test.py hparams/hyperconformer.yaml --find_unused_parameters \
         --seconds_per_batch 100 --train_num_buckets 50 \
         --grad_accumulation_factor 1 --precision fp16 --optimizer_step_limit 1000 \
-        --sim_test_time $sim_test_time --sim_batch_size 10 \
+        --sim_test_time $sim_test_time --sim_batch_size 2 \
         --log_interval 100 \
         --output_folder results/toy/hc/
     rm -r results/toy/hc/save
@@ -44,7 +71,7 @@ do
         memory_test.py hparams/hyperbranchformer.yaml --find_unused_parameters \
         --seconds_per_batch 100 --train_num_buckets 50 \
         --grad_accumulation_factor 1 --precision fp16 --optimizer_step_limit 1000 \
-        --sim_test_time $sim_test_time --sim_batch_size 10 \
+        --sim_test_time $sim_test_time --sim_batch_size 2 \
         --log_interval 100 \
         --output_folder results/toy/hbf/
     rm -r results/toy/hbf/save
@@ -57,7 +84,7 @@ do
         memory_test.py hparams/brq_lg.yaml --find_unused_parameters \
         --seconds_per_batch 100 --train_num_buckets 50 \
         --grad_accumulation_factor 1 --precision fp16 --optimizer_step_limit 1000 \
-        --sim_test_time $sim_test_time --sim_batch_size 10 \
+        --sim_test_time $sim_test_time --sim_batch_size 2 \
         --log_interval 100 \
         --output_folder results/toy/brq_lg/
     rm -r results/toy/brq_lg/save
@@ -69,20 +96,20 @@ do
         memory_test.py hparams/hyperconformer_lg.yaml --find_unused_parameters \
         --seconds_per_batch 100 --train_num_buckets 50 \
         --grad_accumulation_factor 1 --precision fp16 --optimizer_step_limit 1000 \
-        --sim_test_time $sim_test_time --sim_batch_size 10 \
+        --sim_test_time $sim_test_time --sim_batch_size 2 \
         --log_interval 100 \
         --output_folder results/toy/hc_lg/
     rm -r results/toy/hc_lg/save
 done
 
-for sim_test_time in 10 20 30 40 50 60
-do
-    torchrun --nproc_per_node=4 --rdzv_backend c10d --rdzv-endpoint=localhost:0 \
-        memory_test.py hparams/hyperbranchformer_lg.yaml --find_unused_parameters \
-        --seconds_per_batch 100 --train_num_buckets 50 \
-        --grad_accumulation_factor 1 --precision fp16 --optimizer_step_limit 1000 \
-        --sim_test_time $sim_test_time --sim_batch_size 10 \
-        --log_interval 100 \
-        --output_folder results/toy/hbf_lg/
-    rm -r results/toy/hbf_lg/save
-done
+# for sim_test_time in 10 20 30 40 50 60
+# do
+#     torchrun --nproc_per_node=4 --rdzv_backend c10d --rdzv-endpoint=localhost:0 \
+#         memory_test.py hparams/hyperbranchformer_lg.yaml --find_unused_parameters \
+#         --seconds_per_batch 100 --train_num_buckets 50 \
+#         --grad_accumulation_factor 1 --precision fp16 --optimizer_step_limit 1000 \
+#         --sim_test_time $sim_test_time --sim_batch_size 10 \
+#         --log_interval 100 \
+#         --output_folder results/toy/hbf_lg/
+#     rm -r results/toy/hbf_lg/save
+# done
