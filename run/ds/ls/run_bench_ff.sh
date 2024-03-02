ad96e0dc54096613404d04bdb77a67dda024d195
whettenr <whettenrmw@gmail.com>
2024-03-02 12:06:48 +0100

    update ckpt and stuff

--- run/ds/ls/run_bench_ff.sh
+++ run/ds/ls/run_bench_ff.sh
@@ -12,12 +12,12 @@ module load pytorch-gpu/py3/2.1.1
 conda activate aa
 cd /gpfswork/rech/nkp/uaj64gk/attention_alt/brq-att-alt-exp
 
-hub=/gpfswork/rech/nkp/uaj64gk/attention_alt/brq-att-alt-exp/results/old/ff/1000/save/CKPT+2024-02-01+04-59-34+00
+hub=/gpfswork/rech/nkp/uaj64gk/attention_alt/brq-att-alt-exp/results/ff_test/conf_upheads_sm/300epoch_test
 num_layers='13'
-encoder_dim='512' # change to ??
+encoder_dim='624'
 attention_type='fastattention'
 encoder_module='conformer'
-output_folder='results/MP3/ff'
+output_folder='results/MP3/ff_upheads_sm'
 csv_location=/gpfswork/rech/nkp/uaj64gk/attention_alt/brq-att-alt-exp/results/MP3S
 benchmark_location=/gpfswork/rech/nkp/uaj64gk/attention_alt/benchmarks
 
@@ -31,13 +31,13 @@ for i in "${!ConsideredTasks[@]}"; do
 	downstream=${DownStreams[i]}
 	dataset_folder=${DatasetsFolders[i]}
 	python $benchmark_location/benchmarks/MP3S/$task/$downstream/train.py $benchmark_location/benchmarks/MP3S/$task/$downstream/hparams/ssl_brq.yaml \
-		--num_layers_ssl $num_layers --ssl_hub $hub --encoder_dim $encoder_dim --output_folder $output_folder/$task/$downstream --data_folder $dataset_folder --debug \
+		--num_layers_ssl $num_layers --ssl_hub $hub --encoder_dim $encoder_dim --output_folder $output_folder/$task/$downstream --data_folder $dataset_folder \
 		--attention_type $attention_type --encoder_module $encoder_module \
-		--csv_location $csv_location
+		--csv_location $csv_location --nhead 16
 	
-	# python $benchmark_location/benchmarks/MP3S/$task/$downstream/train.py $benchmark_location/benchmarks/MP3S/$task/$downstream/hparams/ssl_brq.yaml \
-	# 	--num_layers_ssl $num_layers --ssl_hub $hub --encoder_dim $encoder_dim --output_folder $output_folder/$task/$downstream --data_folder $dataset_folder \
-	# 	--attention_type $attention_type --encoder_module $encoder_module \
-	# 	--test_only --language_modelling True \
-	# 	--csv_location $csv_location
+	python $benchmark_location/benchmarks/MP3S/$task/$downstream/train.py $benchmark_location/benchmarks/MP3S/$task/$downstream/hparams/ssl_brq.yaml \
+		--num_layers_ssl $num_layers --ssl_hub $hub --encoder_dim $encoder_dim --output_folder $output_folder/$task/$downstream --data_folder $dataset_folder \
+		--attention_type $attention_type --encoder_module $encoder_module \
+		--test_only --language_modelling True \
+		--csv_location $csv_location --nhead 16
 done
