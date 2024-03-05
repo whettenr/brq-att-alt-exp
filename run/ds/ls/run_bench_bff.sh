0dce7d8659871a53a4c0bbbf9cabf4473bbcf159
whettenr <whettenrmw@gmail.com>
2024-03-05 16:03:50 +0100

    add bff librispeech run file

---
+++ run/ds/ls/run_bench_bff.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#SBATCH --job-name=ls_hbf   # nom du job
+#SBATCH --account=dha@v100
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=8
+#SBATCH -C v100-32g
+#SBATCH --qos=qos_gpu-t4
+#SBATCH --time=20:00:00          # temps d'exécution maximum demande (HH:MM:SS) 
+#SBATCH --output=log/hbf_ls_%j.log  # log file
+
+module load pytorch-gpu/py3/2.1.1
+conda activate aa
+cd /gpfswork/rech/nkp/uaj64gk/attention_alt/brq-att-alt-exp
+
+
+hub=/gpfswork/rech/nkp/uaj64gk/attention_alt/brq-att-alt-exp/results/bff_lg/1000/CKPT+2024-03-04+03-16-21+00
+num_layers='25'
+num_encoder_layers='24'
+encoder_dim='1184'
+attention_type='fastattention'
+encoder_module='branchformer'
+output_folder='results/MP3/bff_lg'
+csv_location=/gpfswork/rech/nkp/uaj64gk/attention_alt/brq-att-alt-exp/results/MP3S
+benchmark_location=/gpfswork/rech/nkp/uaj64gk/attention_alt/benchmarks
+
+DatasetsFolders=('/corpus/LibriSpeech/')
+ConsideredTasks=('LibriSpeech')
+DownStreams=('contextnet')
+
+for i in "${!ConsideredTasks[@]}"; do
+	task=${ConsideredTasks[i]}
+	downstream=${DownStreams[i]}
+	dataset_folder=${DatasetsFolders[i]}
+	python $benchmark_location/benchmarks/MP3S/$task/$downstream/train.py $benchmark_location/benchmarks/MP3S/$task/$downstream/hparams/ssl_brq.yaml \
+		--num_layers_ssl $num_layers --num_encoder_layers $num_encoder_layers --ssl_hub $hub --encoder_dim $encoder_dim \
+		--output_folder $output_folder/$task/$downstream --data_folder $dataset_folder \
+		--attention_type $attention_type --encoder_module $encoder_module \
+		--csv_location $csv_location
+	
+	python $benchmark_location/benchmarks/MP3S/$task/$downstream/train.py $benchmark_location/benchmarks/MP3S/$task/$downstream/hparams/ssl_brq.yaml \
+		--num_layers_ssl $num_layers --num_encoder_layers $num_encoder_layers --ssl_hub $hub --encoder_dim $encoder_dim \
+		--output_folder $output_folder/$task/$downstream --data_folder $dataset_folder --test_only --language_modelling True \
+		--attention_type $attention_type --encoder_module $encoder_module \
+		--csv_location $csv_location
+done
