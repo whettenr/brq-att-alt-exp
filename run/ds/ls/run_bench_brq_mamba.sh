9fcd20f5a4f6ffe5e7194ceaaabd31db3c2f1d39
Adel Moumen <adelmoumen.pro@gmail.com>
2024-06-12 12:53:50 +0200

    update yamls

---
+++ run/ds/ls/run_bench_brq_mamba.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#SBATCH --job-name=ls_brq_mamba   # nom du job
+#SBATCH --account=uul@v100
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=8
+#SBATCH --time=20:00:00          # temps d'exécution maximum demande (HH:MM:SS) 
+#SBATCH --output=brq_ls_%j.log  # log file
+
+module purge
+module load cpuarch/amd
+module load pytorch-gpu/py3/2.2.0
+module load ffmpeg/4.2.2
+conda deactivate
+conda activate mamba_ssl
+
+cd /gpfswork/rech/uul/ujg45iy/projects/mamba_ssl/brq-att-alt-exp
+hub=/gpfsscratch/rech/uul/ujg45iy/brq_mamba_bidirectional/save/CKPT+2024-06-07+17-55-16+00
+num_layers=13
+encoder_dim=474
+output_folder='/gpfsscratch/rech/uul/ujg45iy/FT/LS-MP3S/brq_mamba_bidir'
+benchmark_location=/gpfswork/rech/uul/ujg45iy/projects/mamba_ssl/benchmarks
+
+DatasetsFolders=("/gpfsdswork/dataset/LibriSpeech", "/gpfsdswork/dataset/LibriSpeech")
+ConsideredTasks=('LibriSpeech' 'LibriSpeech')
+DownStreams=('LSTM' 'contextnet')
+
+for i in "${!ConsideredTasks[@]}"; do
+	task=${ConsideredTasks[i]}
+	downstream=${DownStreams[i]}
+	dataset_folder=${DatasetsFolders[i]}
+	python $benchmark_location/benchmarks/MP3S/$task/$downstream/train.py $benchmark_location/benchmarks/MP3S/$task/$downstream/hparams/ssl_brq.yaml \
+		--num_layers_ssl $num_layers --ssl_hub $hub --encoder_dim $encoder_dim 
+		--output_folder $output_folder/$task/$downstream --data_folder $dataset_folder \
+	
+	python $benchmark_location/benchmarks/MP3S/$task/$downstream/train.py $benchmark_location/benchmarks/MP3S/$task/$downstream/hparams/ssl_brq.yaml \
+		--num_layers_ssl $num_layers --ssl_hub $hub --encoder_dim $encoder_dim \
+		--output_folder $output_folder/$task/$downstream --data_folder $dataset_folder --test_only --language_modelling True \
+    	--ngram_lm_path $ngram 
+done
+
--- run/ds/ls/run_bench_brq_mamba_lg.sh
+++ run/ds/ls/run_bench_brq_mamba_lg.sh
@@ -1,34 +1,36 @@
 #!/bin/bash
-#SBATCH --job-name=ls_brq_lg_mamba   # nom du job
-#SBATCH --account=dha@v100
+#SBATCH --job-name=ls_brq_lg_mamba
+#SBATCH --account=uul@v100
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=8
-#SBATCH -C v100-32g
-#SBATCH --qos=qos_gpu-t4
-#SBATCH --time=75:00:00          # temps d'exécution maximum demande (HH:MM:SS) 
-#SBATCH --output=log/brq_lg_ls_%j.log  # log file
+#SBATCH --time=20:00:00          # temps d'exécution maximum demande (HH:MM:SS) 
+#SBATCH --output=brq_lg_ls_%j.log  # log file
+#SBATCH --array=0-4%1
 
-module load pytorch-gpu/py3/2.1.1
-conda activate aa
-cd /gpfswork/rech/nkp/uaj64gk/attention_alt/brq-att-alt-exp
+module purge
+module load cpuarch/amd
+module load pytorch-gpu/py3/2.2.0
+module load ffmpeg/4.2.2
+conda deactivate
+conda activate mamba_ssl
 
-hub=/gpfswork/rech/nkp/uaj64gk/attention_alt/brq-att-alt-exp/results/brq_lg/1000/save/CKPT+2024-02-28+09-05-17+00
-num_layers='25'
-num_encoder_layers='24'
-encoder_dim='848'
-output_folder='results/MP3/brq_lg'
-csv_location=/gpfswork/rech/nkp/uaj64gk/attention_alt/brq-att-alt-exp/results/MP3S
-benchmark_location=/gpfswork/rech/nkp/uaj64gk/attention_alt/benchmarks
-ngram='/gpfsscratch/rech/uul/ujg45iy/ngram/ls/4-gram.arpa'
+cd /gpfswork/rech/uul/ujg45iy/projects/mamba_ssl/brq-att-alt-exp
+hub=/gpfsscratch/rech/uul/ujg45iy/brq_mamba_bidirectional_lg/save/CKPT+2024-06-09+18-01-16+00
+num_layers=25
+num_encoder_layers=24
+encoder_dim=678
+output_folder='/gpfsscratch/rech/uul/ujg45iy/FT/LS-MP3S/brq_mamba_bidir_lg'
+benchmark_location=/gpfswork/rech/uul/ujg45iy/projects/mamba_ssl/benchmarks
 
-DatasetsFolders=('/corpus/LibriSpeech/' '/corpus/LibriSpeech/')
+DatasetsFolders=("/gpfsdswork/dataset/LibriSpeech", "/gpfsdswork/dataset/LibriSpeech")
 ConsideredTasks=('LibriSpeech' 'LibriSpeech')
-DownStreams=('contextnet' 'LSTM')
+DownStreams=('LSTM' 'contextnet')
 
 for i in "${!ConsideredTasks[@]}"; do
 	task=${ConsideredTasks[i]}
 	downstream=${DownStreams[i]}
 	dataset_folder=${DatasetsFolders[i]}
+	
 	python $benchmark_location/benchmarks/MP3S/$task/$downstream/train.py $benchmark_location/benchmarks/MP3S/$task/$downstream/hparams/ssl_brq.yaml \
 		--num_layers_ssl $num_layers --num_encoder_layers $num_encoder_layers --ssl_hub $hub --encoder_dim $encoder_dim 
 		--output_folder $output_folder/$task/$downstream --data_folder $dataset_folder \
@@ -36,5 +38,7 @@ for i in "${!ConsideredTasks[@]}"; do
 	python $benchmark_location/benchmarks/MP3S/$task/$downstream/train.py $benchmark_location/benchmarks/MP3S/$task/$downstream/hparams/ssl_brq.yaml \
 		--num_layers_ssl $num_layers --num_encoder_layers $num_encoder_layers --ssl_hub $hub --encoder_dim $encoder_dim \
 		--output_folder $output_folder/$task/$downstream --data_folder $dataset_folder --test_only --language_modelling True \
-    	--ngram_lm_path $ngram 
+    	--ngram_lm_path $ngram \
+    	--test_only 
 done
+
