494f296ad3c103c19193de5c78a6d955c16f8d54
Adel Moumen <adelmoumen.pro@gmail.com>
2024-06-12 18:24:16 +0200

    update slurp mamba

--- run/ds/slurp/run_bench_brq.sh
+++ run/ds/slurp/run_bench_brq.sh
@@ -19,7 +19,7 @@ output_folder='results/MP3/brq'
 csv_location=/gpfswork/rech/nkp/uaj64gk/attention_alt/brq-att-alt-exp/results/MP3S/csv/SLURP
 benchmark_location=/gpfswork/rech/nkp/uaj64gk/attention_alt/benchmarks
 
-DatasetsFolders=('/gpfsscratch/rech/nkp/uaj64gk/corpus/SLURP' '/gpfsscratch/rech/nkp/uaj64gk/corpus/SLURP')
+DatasetsFolders=('/gpfsscratch/rech/uul/ujg45iy/SLURP/slurp/audio' '/gpfsscratch/rech/uul/ujg45iy/SLURP/slurp/audio')
 ConsideredTasks=('SLURP' 'SLURP')
 DownStreams=('LSTM_linear' 'linear')
 
@@ -28,7 +28,10 @@ for i in "${!ConsideredTasks[@]}"; do
 	downstream=${DownStreams[i]}
 	dataset_folder=${DatasetsFolders[i]}
 	python $benchmark_location/benchmarks/MP3S/$task/$downstream/train.py $benchmark_location/benchmarks/MP3S/$task/$downstream/hparams/ssl_brq.yaml \
-		--num_layers_ssl $num_layers --ssl_hub $hub --encoder_dim $encoder_dim --output_folder $output_folder/$task/$downstream --data_folder $dataset_folder \
-		--csv_location $csv_location
+		--num_layers_ssl $num_layers \
+		--ssl_hub $hub \
+		--encoder_dim $encoder_dim \
+		--output_folder $output_folder/$task/$downstream \
+		--data_folder $dataset_folder 
 done
 
---
+++ run/ds/slurp/run_bench_brq_mamba_lg.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#SBATCH --job-name=ls_brq_lg_mamba
+#SBATCH --account=uul@v100
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=8
+#SBATCH --time=20:00:00          # temps d'exécution maximum demande (HH:MM:SS) 
+#SBATCH --output=brq_lg_ls_%j.log  # log file
+#SBATCH --array=0-4%1
+
+module purge
+module load cpuarch/amd
+module load pytorch-gpu/py3/2.2.0
+module load ffmpeg/4.2.2
+conda deactivate
+conda activate mamba_ssl
+
+cd /gpfswork/rech/uul/ujg45iy/projects/mamba_ssl/brq-att-alt-exp
+hub=/gpfsscratch/rech/uul/ujg45iy/brq_mamba_bidirectional_lg/save/CKPT+2024-06-09+18-01-16+00
+num_layers=25
+num_encoder_layers=24
+encoder_dim=678
+output_folder='/gpfsscratch/rech/uul/ujg45iy/FT/LS-MP3S/brq_mamba_bidir_lg'
+benchmark_location=/gpfswork/rech/uul/ujg45iy/projects/mamba_ssl/benchmarks
+
+DatasetsFolders=("/gpfsdswork/dataset/LibriSpeech" "/gpfsdswork/dataset/LibriSpeech")
+ConsideredTasks=('LibriSpeech' 'LibriSpeech')
+DownStreams=('LSTM' 'contextnet')
+
+for i in "${!ConsideredTasks[@]}"; do
+	task=${ConsideredTasks[i]}
+	downstream=${DownStreams[i]}
+	dataset_folder=${DatasetsFolders[i]}
+	
+	python $benchmark_location/benchmarks/MP3S/$task/$downstream/train.py $benchmark_location/benchmarks/MP3S/$task/$downstream/hparams/ssl_brq.yaml \
+		--num_layers_ssl $num_layers \
+		--num_encoder_layers $num_encoder_layers \
+		--ssl_hub $hub \
+		--encoder_dim $encoder_dim \
+		--output_folder $output_folder/$task/$downstream \
+		--data_folder $dataset_folder 
+	
+	python $benchmark_location/benchmarks/MP3S/$task/$downstream/train.py $benchmark_location/benchmarks/MP3S/$task/$downstream/hparams/ssl_brq.yaml \
+		--num_layers_ssl $num_layers \
+		--num_encoder_layers $num_encoder_layers \
+		--ssl_hub $hub \
+		--encoder_dim $encoder_dim \
+		--output_folder $output_folder/$task/$downstream \
+		--data_folder $dataset_folder \
+		--test_only \
+		--language_modelling True \
+    	--ngram_lm_path $ngram \
+    	--test_only 
+done
+
