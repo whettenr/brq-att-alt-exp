ae9dbaeee5ecefa234c2b52e5662a7a534c6e7e9
Adel Moumen <adelmoumen.pro@gmail.com>
2024-06-18 22:41:00 +0200

    update code

--- run/toy_test_adel.sh
+++ run/toy_test_adel.sh
@@ -20,31 +20,6 @@ conda activate mamba_ssl
 cd /gpfswork/rech/uul/ujg45iy/projects/mamba_ssl/brq-att-alt-exp
 
 
-# brq 
-for sim_test_time in 10 20 30 40 50 60 70 80 90
-do
-    torchrun --nproc_per_node=1 --rdzv_backend c10d --rdzv-endpoint=localhost:0 \
-        memory_test.py hparams/brq_mamba_bidirectional.yaml --find_unused_parameters \
-        --seconds_per_batch 100 --train_num_buckets 50 \
-        --grad_accumulation_factor 1 --precision fp16 --optimizer_step_limit 1000 \
-        --sim_test_time $sim_test_time --sim_batch_size 2 \
-        --log_interval 100 \
-        --output_folder $SCRATCH/results/toy/brq/
-    rm -r $SCRATCH/results/toy/brq/save
-done
-
-for sim_test_time in 10 20 30 40 50 60 70 80 90
-do
-    torchrun --nproc_per_node=1 --rdzv_backend c10d --rdzv-endpoint=localhost:0 \
-        memory_test.py hparams/brq_mamba_bidirectional_lg.yaml --find_unused_parameters \
-        --seconds_per_batch 100 --train_num_buckets 50 \
-        --grad_accumulation_factor 1 --precision fp16 --optimizer_step_limit 1000 \
-        --sim_test_time $sim_test_time --sim_batch_size 2 \
-        --log_interval 100 \
-        --output_folder $SCRATCH/results/toy/brq_lg/
-    rm -r $SCRATCH/results/toy/brq_lg/save
-done
-
 
 # fastformer
 for sim_test_time in 10 20 30 40 50 60 70 80 90
